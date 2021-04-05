e645c6b394b0f70e51430d0767d215ea53e63551
Cuervo <yo.cuervo.yo@gmail.com>
2021-04-05 11:52:53 -0300

    front del backoffice listo

--- src/BackOffice/BackOffice.js
+++ src/BackOffice/BackOffice.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 import '../BackOffice/Styles/BackOffice.css';
 
+function irArriba(){
+    window.scrollTo({top:0, behavior: 'smooth'});
+  }
+
 function BackOffice() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-4 marco__izq">
-                    <button className="btn btn-primary boton">Nuevos Productos</button>
-                        <br/>
-                    <button className="btn btn-info">Productos</button>
-                </div>
-                <div className="col marco__der">     
-                    <div className="visualizador">
-
-                    </div>
-                    <div className="botones">
-                        <button className="btn btn-danger mr-3">ELIMINAR</button>
-                        <button className="btn btn-info mr-3">CANCELAR</button>
-                        <button className="btn btn-success mr-3">GUARDAR</button>
-                    </div>
+        <div className="container__back">
+            <div className="row header">
+                <div className="col titulo">PRODUCTOS</div>
+                <div className="col">
+                    <button className="btn btn-primary boton__header">Nuevo Producto</button>
                 </div>
             </div>
+            <div className="row contenedor">
+                <div className="col">
+                    <div className="producto"></div>
+                    <button className="btn btn-info  ml-2 mr-2">Modificar</button>
+                    <button className="btn btn-danger">Eliminar</button>
+                </div>
+                <div className="col">
+                    <div className="producto"></div>
+                    <button className="btn btn-info  ml-2 mr-2">Modificar</button>
+                    <button className="btn btn-danger">Eliminar</button>
+                </div>
+            </div>
+            <button onClick={irArriba} className="btn btn-outline-info btn-lg btn-block mt-2 mb-2">Volver Arriba</button>
         </div>
     );
 }
