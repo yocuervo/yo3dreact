a1d14718513529ddc8c444f6b6c6bb63dd2ee756
Cuervo <yo.cuervo.yo@gmail.com>
2021-06-24 08:50:23 -0300

    cree llamada a Api y un puerto falso para prueba local en el port 3001

---
+++ src/Api.js
@@ -0,0 +1,45 @@
+const BASE_URL = 'http://localhost:3001';
+
+async function callApi(endpoint, options = {}) {
+
+    options.headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    };
+
+    const url = BASE_URL + endpoint;
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
+
+const api = {
+    modelos: {
+        list() {
+            return callApi('/modelslist');
+        },
+        create(modelo) {
+            return callApi(`/modelslist`, {
+                method: 'POST',
+                body: JSON.stringify(modelo),
+            });
+        },
+        read(modeloId) {
+            return callApi(`/modelslist/${modeloId}`);
+        },
+        update(modeloId, updates) {
+            return callApi(`/modelslist/${modeloId}`, {
+                method: 'PUT',
+                body: JSON.stringify(updates),
+            });
+        },
+        remove(modeloId) {
+            return callApi(`/modelslist/${modeloId}`, {
+                method: 'DELETE',
+            });
+        },
+    },
+};
+
+export default api;
--- src/components/Modelos.js
+++ src/components/Modelos.js
@@ -1,40 +1,67 @@
 import React from 'react';
 
 import './styles/Modelos.css';
-import imgUnoUno from '../images/modelos/imgUnoUno.jpg';
+import PageLoading from './PageLoading';
+import api from '../Api';
 
 class Modelos extends React.Component {
     state = {
+        loading: true,
+        error: null,
         data: {
-            nombre: "Exequiel",
-            alto: "220",
-            ancho: "50",
-            largo: "30",
-            precio: "2500",
-            creadorModelo: "thingiverse",
-            artistaModelo: "Ivana"
+            img: "",
+            nombre: "",
+            alto: "",
+            ancho: "",
+            largo: "",
+            precio: "",
+            creadorModelo: "",
+            artistaModelo: ""
+        }
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        this.setState({ loading: true, error: null });
+
+        try {
+            const data = await api.modelos.list();
+            this.setState({ loading: false, data: data });
+        } catch (error) {
+            this.setState({ loading: false, error: error });
         }
     }
 
     render() {
+        if (this.state.loading === true) {
+            return <PageLoading />;
+        }
+
+        if (this.state.error) {
+            return `Error: ${this.state.error.message}`;
+        }
+
         return (
 
             <div className="row row-cols-1 row-cols-md-2 modelos__row">
 
-                    <div className="col modelos__col">
-                        <img src={imgUnoUno} className="modelos__img" alt="modelo"/>
-                        <h5>Nombre: {this.state.data.nombre}</h5>
-                        <h5>Alto: {this.state.data.alto}cm</h5>
-                        <h5>Ancho: {this.state.data.ancho}cm</h5>
-                        <h5>Largo: {this.state.data.largo}cm</h5>
-                        <h5>Precio: ${this.state.data.precio}</h5>
-                        <h5>Creador del Modelo</h5>
-                        <h5>{this.state.data.creadorModelo}</h5>
-                        <h5>Pintor del Modelo</h5>
-                        <h5>{this.state.data.artistaModelo}</h5>
-                    </div>
-
+                <div className="col modelos__col">
+                    <img src={this.state.data.img} alt="Imagen del Modelo" />
+                    <h5>Nombre: {this.state.data.nombre}</h5>
+                    <h5>Alto: {this.state.data.alto}cm</h5>
+                    <h5>Ancho: {this.state.data.ancho}cm</h5>
+                    <h5>Largo: {this.state.data.largo}cm</h5>
+                    <h5>Precio: ${this.state.data.precio}</h5>
+                    <h5>Creador del Modelo</h5>
+                    <h5>{this.state.data.creadorModelo}</h5>
+                    <h5>Pintor del Modelo</h5>
+                    <h5>{this.state.data.artistaModelo}</h5>
                 </div>
+
+            </div>
         )
     }
 }
